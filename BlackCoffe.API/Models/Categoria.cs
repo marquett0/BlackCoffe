4ca680c1d4a19ee95cdda1797cf1a554ae5d8930
lucasSouza225 <lucas2469y@gmail.com>
2025-11-26 22:36:46 -0300

    ajustando a cor

--- BlackCoffe.API/Models/Categoria.cs
+++ BlackCoffe.API/Models/Categoria.cs
@@ -1,23 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlackCoffe.API.Models;
 
 [Table("Categoria")]
-    public class Categoria
+public class Categoria
 {
-    [Key]
-    public int Id { get; set; } 
 
-    [StringLength(30)]
-    [Required(ErrorMessage = "O Nome é obrigatório")]
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string Nome { get; set; }
 
     [StringLength(300)]
     public string Foto { get; set; }
 
-    
-    [StringLength(26) ]
-    public string Cor { get; set; }
-
-
+    [StringLength(26)]
+    public string Cor { get; set; } = "rgba(0,0,0,1)";
 }
